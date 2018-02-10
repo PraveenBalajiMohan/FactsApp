d35357f571a320c85a0e6bbd8f36aace4fa2db11
Praveen Balaji <praveen89balaji@gmail.com>
2018-02-11 01:42:30 +0530

    Initial commit

---
+++ app/src/main/java/com/wipro/android/facts/FactsActivity.java
@@ -0,0 +1,13 @@
+package com.wipro.android.facts;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FactsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_facts);
+    }
+}
